015ddbb16c16425d97c0c5bcade0cb038ef5a8a2
ali-ben-hammad <ali.benhammad-etu@etu.univh2c.ma>
2023-12-24 11:11:42 +0100

    update register and login design

---
+++ client/src/main/java/com/projet/client/RegisterController.java
@@ -0,0 +1,16 @@
+package com.projet.client;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RegisterController {
+
+
+}
