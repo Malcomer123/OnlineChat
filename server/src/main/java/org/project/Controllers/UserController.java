016321006115ecca4d30caa947efff83343a33ed
malcomer123 <achrafhammi29@gmail.com>
2024-01-07 18:11:20 +0100

    server

--- server/src/main/java/org/project/Controllers/UserController.java
+++ server/src/main/java/org/project/Controllers/UserController.java
@@ -1,4 +1,32 @@
 package org.project.Controllers;
 
+import java.io.*;
+import java.net.Socket;
+
 public class UserController {
+    private ObjectInputStream is;
+    private ObjectOutputStream os;
+
+    public UserController(Socket socket) {
+        try {
+            InputStream inputStream = socket.getInputStream();
+            is = new ObjectInputStream(inputStream);
+            OutputStream outputStream = socket.getOutputStream();
+            os = new ObjectOutputStream(outputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean authentication() throws IOException, ClassNotFoundException {
+        try {
+            String username = (String) is.readObject();
+            String password = (String) is.readObject();
+            System.out.println(username + password);
+            return false;
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
